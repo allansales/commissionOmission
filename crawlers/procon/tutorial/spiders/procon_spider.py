a5783c75af1e7f52b904a8aa18301d0a01b82759
allansales <allan.sales.89@gmail.com>
2019-12-09 17:33:14 +0100

    crawlers done. metric developmente in progress

--- crawlers/procon/tutorial/spiders/procon_spider.py
+++ crawlers/procon/tutorial/spiders/procon_spider.py
@@ -20,11 +20,19 @@ class AuthorSpider(scrapy.Spider):
 	"https://minimum-wage.procon.org/","https://usiraq.procon.org/view.resource.php?resourceID=000668","https://immigration.procon.org/view.resource.php?resourceID=000842"
 	]
 
+	topic_urls = ["Death Penalty", "Gun Control", "Marijuana", "ACLU", "Euthanasia", "Abortion", "Health Care Form", "Climate Change", "Net Neutrality", "GMOs", "Corporal Punishment",
+				  "School Vouchers", "Gay Marriage", "Prostitution", "Video Games", "Minimum Wage", "Iraq War", "Immigration"]
+
 	# Get information whether the argument is Pro or Con the subject
 	def get_pro_con(self, pro_args, con_args):
 		pro = ["Pro "+ str(x) for x in range(1, len(pro_args)+1)]
 		con = ["Con "+ str(x) for x in range(1, len(con_args)+1)]
 		return pro + con
+
+	def get_topic(self, response):
+		url = response.request.url
+		idx = self.start_urls.index(url)
+		return self.topic_urls[idx]
 
 	# manually remove persistent tags and unwanted strings from text
 	def preprocess_text(self, str_array):
@@ -32,12 +40,20 @@ class AuthorSpider(scrapy.Spider):
 		new_str_array = []
 		# detect tags
 		tags_reg = re.compile('<.*?>')
+
 		# detect pro con string
 		procon_reg = re.compile('Pro [\d]+|Con [\d]+')
+
 		# detect citation
 		citation_reg = re.compile('\[\d+\]')
+
+		# detect brackets. i.e., [ or ]
+		bracket_reg = re.compile('[\[\]]')
+
 		for string in str_array:
 			string = re.sub(tags_reg, '', string)
+			string = re.sub(bracket_reg, '', string)
+			string = re.sub(citation_reg, '', string)
 			string = re.sub(procon_reg, '', string, 1).strip()
 			new_str_array.append(string)
 		return new_str_array
@@ -67,9 +83,10 @@ class AuthorSpider(scrapy.Spider):
 
 		return {
 			'url': response.request.url,
+			'topic': self.get_topic(response),
 			'pro_con': self.get_pro_con(pro_args, con_args),
 			'argument_topic': topic + topic,
-			'argument': self.preprocess_text(pro_args) + self.preprocess_text(con_args) # needs to remove the tags from the content (recreational marijuana)
+			'argument': self.preprocess_text(pro_args) + self.preprocess_text(con_args)
 		}
 
 	def parse_paired_args_by_one_topic(self, response):
@@ -79,6 +96,7 @@ class AuthorSpider(scrapy.Spider):
 
 		return {
 			'url': response.request.url,
+			'topic': self.get_topic(response),
 			'pro_con': self.get_pro_con(pro_args, con_args),
 			'argument': self.preprocess_text(pro_args) + self.preprocess_text(con_args)
 		}
@@ -90,9 +108,10 @@ class AuthorSpider(scrapy.Spider):
 	def parse_unpaired_args_page(self, response):
 		return {
 			'url': response.request.url,
+			'topic': self.get_topic(response),
 			'pro_con': response.xpath('//div[@class="argument-container"]/blockquote/h3/text()').getall(),
 			'argument_summary': response.xpath('//div[@class="argument-container"]/blockquote/h4/text()').getall(),
-			'argument': self.preprocess_text(response.xpath('//div[@class="argument-container"]//blockquote').getall()) # needs to remove the tags from the content (recreational marijuana)
+			'argument': self.preprocess_text(response.xpath('//div[@class="argument-container"]//blockquote').getall())
 		}
 
 	def parser_chooser(self, response):
--- crawlers/procon/tutorial/spiders/wikis_spider.py
+++ crawlers/procon/tutorial/spiders/wikis_spider.py
@@ -4,13 +4,29 @@ import re
 class AuthorSpider(scrapy.Spider):
     name = 'wiki'
 
-    start_urls = ["https://www.conservapedia.com/Capital_punishment","https://www.conservapedia.com/Gun#Gun_control","https://www.conservapedia.com/Marijuana","https://www.conservapedia.com/ACLU","https://www.conservapedia.com/Euthanasia","https://www.conservapedia.com/Abortion","https://www.conservapedia.com/ObamaCare","https://www.conservapedia.com/Climate_change","https://www.conservapedia.com/Network_neutrality","https://www.conservapedia.com/Genetically_modified_organism","https://www.conservapedia.com/Corporal_punishment","https://www.conservapedia.com/School_vouchers","https://www.conservapedia.com/Same-sex_marriage","https://www.conservapedia.com/Prostitution","https://www.conservapedia.com/Video_game#Arguments_against_video_game_usage","https://www.conservapedia.com/Minimum_wage","https://www.conservapedia.com/Iraq_War","https://www.conservapedia.com/Illegal_immigration",
-                  "https://rationalwiki.org/wiki/Capital_punishment","https://rationalwiki.org/wiki/Gun_control#Common_arguments_against_gun_control","https://rationalwiki.org/wiki/Cannabis","https://rationalwiki.org/wiki/American_Civil_Liberties_Union","https://rationalwiki.org/wiki/Euthanasia","https://rationalwiki.org/wiki/Abortion","https://rationalwiki.org/wiki/Affordable_Care_Act","https://rationalwiki.org/wiki/Global_warming","https://rationalwiki.org/wiki/Net_neutrality","https://rationalwiki.org/wiki/Genetically_modified_food","https://rationalwiki.org/wiki/Corporal_punishment","https://rationalwiki.org/wiki/School_vouchers","https://rationalwiki.org/wiki/Same-sex_marriage","https://rationalwiki.org/wiki/Prostitution","https://rationalwiki.org/wiki/Video_game#Violence","https://rationalwiki.org/wiki/Minimum_wage","https://rationalwiki.org/wiki/Iraq_War","https://rationalwiki.org/wiki/Illegal_immigration"
-                  #"https://www.conservapedia.com/Marijuana","https://www.conservapedia.com/Network_neutrality","https://www.conservapedia.com/Genetically_modified_organism","https://www.conservapedia.com/Corporal_punishment","https://www.conservapedia.com/Prostitution","https://rationalwiki.org/wiki/Capital_punishment","https://rationalwiki.org/wiki/Net_neutrality","https://rationalwiki.org/wiki/Genetically_modified_food","https://rationalwiki.org/wiki/School_vouchers","https://rationalwiki.org/wiki/Minimum_wage"
-                  ]
+    conservapedia_urls = ["https://www.conservapedia.com/Capital_punishment","https://www.conservapedia.com/Gun#Gun_control","https://www.conservapedia.com/Marijuana",
+                          "https://www.conservapedia.com/ACLU","https://www.conservapedia.com/Euthanasia","https://www.conservapedia.com/Abortion",
+                          "https://www.conservapedia.com/ObamaCare","https://www.conservapedia.com/Climate_change","https://www.conservapedia.com/Network_neutrality",
+                          "https://www.conservapedia.com/Genetically_modified_organism","https://www.conservapedia.com/Corporal_punishment","https://www.conservapedia.com/School_vouchers",
+                          "https://www.conservapedia.com/Same-sex_marriage","https://www.conservapedia.com/Prostitution",
+                          "https://www.conservapedia.com/Video_game#Arguments_against_video_game_usage","https://www.conservapedia.com/Minimum_wage",
+                          "https://www.conservapedia.com/Iraq_War","https://www.conservapedia.com/Illegal_immigration"]
+    rationalwiki_urls = ["https://rationalwiki.org/wiki/Capital_punishment","https://rationalwiki.org/wiki/Gun_control#Common_arguments_against_gun_control",
+                         "https://rationalwiki.org/wiki/Cannabis","https://rationalwiki.org/wiki/American_Civil_Liberties_Union",
+                         "https://rationalwiki.org/wiki/Euthanasia","https://rationalwiki.org/wiki/Abortion","https://rationalwiki.org/wiki/Affordable_Care_Act",
+                         "https://rationalwiki.org/wiki/Global_warming","https://rationalwiki.org/wiki/Net_neutrality","https://rationalwiki.org/wiki/Genetically_modified_food",
+                         "https://rationalwiki.org/wiki/Corporal_punishment","https://rationalwiki.org/wiki/School_vouchers","https://rationalwiki.org/wiki/Same-sex_marriage",
+                         "https://rationalwiki.org/wiki/Prostitution","https://rationalwiki.org/wiki/Video_game#Violence","https://rationalwiki.org/wiki/Minimum_wage",
+                         "https://rationalwiki.org/wiki/Iraq_War","https://rationalwiki.org/wiki/Illegal_immigration"]
+
+    topic_urls = ["Death Penalty", "Gun Control", "Marijuana", "ACLU", "Euthanasia", "Abortion", "Health Care Form", "Climate Change", "Net Neutrality", "GMOs", "Corporal Punishment",
+                  "School Vouchers", "Gay Marriage", "Prostitution", "Video Games", "Minimum Wage", "Iraq War", "Immigration"]
+
+    start_urls = conservapedia_urls + rationalwiki_urls
 
     def preprocess_text(self, response):
 
+        #str = response.xpath('//div[@class="mw-content-ltr"]/*[not(@class="toc")]').get()
         str = response.xpath('//div[@class="mw-content-ltr"]').get()
 
         # remove tags
@@ -22,17 +38,36 @@ class AuthorSpider(scrapy.Spider):
 
         # remove brackets texts. e.g., [1], [edit], [cite] or [citation NOT needed]
         bracket_reg = re.compile('\[\w+\s*\w*\s*\w*\]')
-        #bracket_reg = re.compile('\[(\w+\s*\w*\d*/*:*)+\]')
         str = re.sub(bracket_reg, '', str)
 
         # split text by paragraph
-        paragraph = str.split("\n")
+        paragraphs = str.split("\n")
 
-        # delete text after the "See Also" section
-        idx = paragraph.index("References")
-        paragraph = paragraph[:idx]
+        # delete text after the "See Also" section, "Further reading" or "References"
+        if "See also" in paragraphs:
+            idx = paragraphs.index("See also")
+            paragraphs = paragraphs[:idx]
 
-        return paragraph
+        if "Further reading" in paragraphs:
+            idx = paragraphs.index("Further reading")
+            paragraphs = paragraphs[:idx]
+
+        if "References" in paragraphs:
+            idx = paragraphs.index("References")
+            paragraphs = paragraphs[:idx]
+
+        return paragraphs
+
+    def get_topic(self, response, source):
+
+        url = response.request.url
+
+        urls = self.rationalwiki_urls
+        if source == "conservapedia":
+            urls = self.conservapedia_urls
+
+        idx = urls.index(url)
+        return self.topic_urls[idx]
 
     def get_source(self, response):
         if "wikipedia" in response.request.url:
@@ -45,9 +80,10 @@ class AuthorSpider(scrapy.Spider):
             return "conservapedia"
 
     def parse(self, response):
-
+        source = self.get_source(response)
         yield {
-            'source': self.get_source(response),
+            'source': source,
+            'topic': self.get_topic(response, source),
             'title': response.xpath('//*[@id="firstHeading"]//text()').get(),
             #'content': response.xpath('//div[@id="bodyContent"]').get()
             'content': self.preprocess_text(response)
